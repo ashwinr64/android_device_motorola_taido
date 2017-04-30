559b25cbc9c0fa62749c284d412da96b1eb27a83
Ashwin Ramesh <ashwinr64@gmail.com>
2017-04-30 22:41:33 +0530

    taido: Satisfy symbols required to get audio HAL loading
    * All credits to Decker for his patch in f/av

--- libmtk_symbols/Android.mk
+++ libmtk_symbols/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
+    mtk_audio.cpp \
     mtk_gui.cpp \
     mtk_ui.cpp \
     mtk_omx.cpp \
@@ -14,19 +15,18 @@ LOCAL_SRC_FILES_32 := mtk_string.cpp
 # only for 64bit libraries
 LOCAL_SRC_FILES_64 := mtk_parcel.cpp
 
+LOCAL_C_INCLUDES += \
+    $(TOP)/frameworks/rs/server \
+    $(TOP)/frameworks/native/include/media/openmax \
+    $(TOP)/frameworks/av/include/media/ \
+    $(TOP)/frameworks/av/media/libstagefright \
+    $(TOP)/frameworks/av/media/libavextensions \
+    $(call include-path-for, audio-effects) \
+    $(call include-path-for, audio-utils)
+
 LOCAL_SHARED_LIBRARIES := libbinder libutils liblog libgui libui \
-                          libicuuc libicui18n libcrypto
+                          libicuuc libicui18n libcrypto libmedia
 LOCAL_MODULE := libmtk_symbols
+LOCAL_CLANG := false
 LOCAL_MODULE_TAGS := optional
-
-
-LOCAL_CLANG := true
-LOCAL_CPPFLAGS := -std=c++1y 
-# LOCAL_SANITIZE := integer
-LOCAL_CPPFLAGS += -Wno-exit-time-destructors
-LOCAL_CPPFLAGS += -Wno-global-constructors
-LOCAL_CPPFLAGS += -Wno-c++98-compat-pedantic
-LOCAL_CPPFLAGS += -Wno-four-char-constants
-LOCAL_CPPFLAGS += -Wno-padded
-
 include $(BUILD_SHARED_LIBRARY)
